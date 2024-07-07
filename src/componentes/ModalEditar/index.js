10b971375fc7b2cad722ac0998ceec427690f346
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-07-06 21:12:07 -0300

    estilizacoes responsabilidade ajustes

--- src/componentes/ModalEditar/index.js
+++ src/componentes/ModalEditar/index.js
@@ -48,67 +48,72 @@ function ModalEditar({ video, aoFechar, aoSalvar, aoFecharModal, categorias }) {
     }
 
     return (
-        <dialog open={!!video} className={styles.modal}>
-            <MdOutlineCancel onClick={aoFecharModal} className={styles.iconeFechar} />
-            <h1>Editar card</h1>
-            <form onSubmit={submit} method="dialog">
-                <div className={styles.sessaoCampos}>
-                    <div className={styles.campos}>
-                        <CampoFormulario className={styles.campoModal}
-                            obrigatorio={true}
-                            label="Título"
-                            placeholder="Insira o título"
-                            valor={titulo}
-                            aoAlterado={valor => setTitulo(valor)}
-                        />
+        <>
+            {video && <>
+                <div className={styles.overley} />
+                <dialog open={!!video} className={styles.modal}>
+                    <MdOutlineCancel onClick={aoFecharModal} className={styles.iconeFechar} />
+                    <h1>Editar card</h1>
+                    <form onSubmit={submit} method="dialog">
+                        <div className={styles.sessaoCampos}>
+                            <div className={styles.campos}>
+                                <CampoFormulario className={styles.campoModal}
+                                    obrigatorio={true}
+                                    label="Título"
+                                    placeholder="Insira o título"
+                                    valor={titulo}
+                                    aoAlterado={valor => setTitulo(valor)}
+                                />
 
-                        <ListaSuspensa
-                            obrigatorio={true}
-                            label="Categoría"
-                            placeholder="Selecione uma categoía..."
-                            itens={categorias}
-                            valor={categoria}
-                            aoAlterado={valor => setCategoria(valor)}
-                        />
+                                <ListaSuspensa
+                                    obrigatorio={true}
+                                    label="Categoría"
+                                    placeholder="Selecione uma categoía..."
+                                    itens={categorias}
+                                    valor={categoria}
+                                    aoAlterado={valor => setCategoria(valor)}
+                                />
 
-                        <CampoFormulario
-                            obrigatorio={true}
-                            label="Imagem"
-                            placeholder="URL da imagem"
-                            valor={imagem}
-                            aoAlterado={valor => setImagem(valor)}
-                        />
+                                <CampoFormulario
+                                    obrigatorio={true}
+                                    label="Imagem"
+                                    placeholder="URL da imagem"
+                                    valor={imagem}
+                                    aoAlterado={valor => setImagem(valor)}
+                                />
 
-                        <CampoFormulario
-                            obrigatorio={true}
-                            label="Vídeo"
-                            placeholder="URL do vídeo"
-                            valor={link}
-                            aoAlterado={valor => setLink(valor)}
-                        />
+                                <CampoFormulario
+                                    obrigatorio={true}
+                                    label="Vídeo"
+                                    placeholder="URL do vídeo"
+                                    valor={link}
+                                    aoAlterado={valor => setLink(valor)}
+                                />
 
-                        <Textarea
-                            obrigatorio={true}
-                            label="Descrição"
-                            placeholder="Sobre o que é esse vídeo?"
-                            valor={descricao}
-                            aoAlterado={valor => setDescricao(valor)}
-                        />
+                                <Textarea
+                                    obrigatorio={true}
+                                    label="Descrição"
+                                    placeholder="Sobre o que é esse vídeo?"
+                                    valor={descricao}
+                                    aoAlterado={valor => setDescricao(valor)}
+                                />
 
-                    </div>
-                    <div className={styles.botoes}>
-                        <BotaoFormulario
-                            children="Guardar"
-                            type='submit'
-                        />
-                        <BotaoFormulario
-                            children="Limpar"
-                            type='reset'
-                        />
-                    </div>
-                </div>
-            </form>
-        </dialog>
+                            </div>
+                            <div className={styles.botoes}>
+                                <BotaoFormulario
+                                    children="Guardar"
+                                    type='submit'
+                                />
+                                <BotaoFormulario
+                                    children="Limpar"
+                                    type='reset'
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </dialog>
+            </>}
+        </>
     )
 }
 
