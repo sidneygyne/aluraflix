d568df371cd859b4a97e63776d86a82e804339f6
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-07-12 15:37:27 -0300

    ajustes menu smartphones

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -4,21 +4,23 @@ import logo from './logo.png'
 import { Botao } from 'componentes/Botao'
 
 function Cabecalho() {
-    
+
     return (
         <header className={styles.cabecalho}>
-            <Link to="./">
-                <img className={styles.logo} src={logo} alt="LogoAluraFlix" />
-            </Link>
+            <>
+                <Link to="./">
+                    <img className={styles.logo} src={logo} alt="LogoAluraFlix" />
+                </Link>
 
-            <nav className={styles.menu}>
-                <Botao condition="true" url="./" >
-                    HOME
-                </Botao>
-                <Botao condition="true" url="./addvideo" >
-                    NOVO VIDEO
-                </Botao>
-            </nav>
+                <nav className={styles.menu}>
+                    <Botao condition="true" url="./" >
+                        HOME
+                    </Botao>
+                    <Botao condition="true" url="./addvideo" >
+                        NOVO VIDEO
+                    </Botao>
+                </nav>
+            </>
         </header>
     )
 }
